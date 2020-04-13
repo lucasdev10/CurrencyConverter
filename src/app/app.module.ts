200f1ca0df46099c87729820d17b7a44db4a0136
lucasdev10 <lucas.adsjr@gmail.com>
2020-04-13 14:21:13 -0300

    Adicionando componente de grafico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 import {  HttpClientModule } from '@angular/common/http';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculadoraComponent
+    CalculadoraComponent,
+    LineChartComponent
   ],
   imports: [
     BrowserModule,
